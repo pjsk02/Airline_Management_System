0a922773af5afdf722087321fd5bcf52cb1254fa
pjsk02 <pulipati.j@northeastern.edu>
2025-05-25 18:52:52 +0530

    Added Temp File

---
+++ one.sql
@@ -0,0 +1,13 @@
+create database proj;
+use proj;
+CREATE TABLE passenger (
+    pid INT,
+    f_name VARCHAR(15),
+    l_name VARCHAR(15),
+    phnum INT,
+    age INT,
+    address VARCHAR(50),
+    aadhar_num INT
+);
+
+
